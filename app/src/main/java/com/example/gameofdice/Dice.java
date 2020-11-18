97e08eb73157751c92ed0c3d2ab27e9df62d0200
MadMakZa <immakza@gmail.com>
2020-11-18 21:17:07 +0300

    redact class Dice

--- app/src/main/java/com/example/gameofdice/Dice.java
+++ app/src/main/java/com/example/gameofdice/Dice.java
@@ -1,4 +1,14 @@
 package com.example.gameofdice;
 
+import android.nfc.Tag;
+import android.util.Log;
+
 public class Dice {
+    //диапазон разброса
+    private static final int A = 1;
+    private static final int B = 6;
+
+    int random_number = A + (int) (Math.random() * B);
+    Log.d("Dice","Кубик выпал на: " );
+
 }
--- app/src/main/java/com/example/gameofdice/StartGame.java
+++ app/src/main/java/com/example/gameofdice/StartGame.java
@@ -1,4 +1,12 @@
 package com.example.gameofdice;
 
 public abstract class StartGame {
+    /***Этапы игры ***/
+
+    //Создать player
+    //Создать computer
+    //Сравнить результаты бросков
+    //Вывод победителя
+    //Завершение и обновление
+
 }
