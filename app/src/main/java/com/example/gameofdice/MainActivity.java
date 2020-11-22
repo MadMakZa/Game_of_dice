a77459cfcb8f2299a2021fe7ffedb552079af2f0
MadMakZa <immakza@gmail.com>
2020-11-22 21:49:29 +0300

    add testAnimation

--- app/src/main/java/com/example/gameofdice/MainActivity.java
+++ app/src/main/java/com/example/gameofdice/MainActivity.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button roll;
+    private Animation cubeAnim;
+    private ImageView cubeImage1;
+    private ImageView cubeImage2;
+    private ImageView cubeImage3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //анимация
+        cubeAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.cube_anim);
+        cubeImage1 = findViewById(R.id.cube_1);
+        //запуск анимации
+        cubeImage1.startAnimation(cubeAnim);
 
         //test cube
         roll = (Button) findViewById(R.id.roll);
