9b0f3f25047c8a75a85810e994a4880651e1364a
MadMakZa <immakza@gmail.com>
2020-11-23 15:03:19 +0300

    Refactor images cubes, its work! add Enum

--- app/src/main/java/com/example/gameofdice/Dice.java
+++ app/src/main/java/com/example/gameofdice/Dice.java
@@ -2,12 +2,16 @@ package com.example.gameofdice;
 
 import android.nfc.Tag;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /* *
  * Игральный кубик, умеет генерировать рандомное число
  * */
 
 public class Dice {
+    private Animation cubeAnim;
     //диапазон разброса
     private final int a = 1;
     private final int b = 6;
@@ -22,4 +26,16 @@ public class Dice {
         return random_number;
     }
 
+    enum DiceVisual {
+
+        ONE {
+            public Animation cubeAnim;
+            private ImageView cubeImage = (ImageView) findViewById(R.id.cube_1);
+
+            public void DiceImageAnimation () {
+                cubeAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.cube_anim);
+                MainActivity.getInstance().getCubeImageView1().setImageResource(R.drawable.cube1);
+            }
+        },
+    }
 }
