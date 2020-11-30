9d639ef8dbeb7d1cab9701f8f8cd19fdd4d4c0fe
MadMakZa <immakza@gmail.com>
2020-11-30 22:59:29 +0300

    add method showPoints in class RollOfTheDice stage log

--- app/src/main/java/com/example/gameofdice/RollOfTheDice.java
+++ app/src/main/java/com/example/gameofdice/RollOfTheDice.java
@@ -58,13 +58,17 @@ public abstract class RollOfTheDice {
 
     //логика подсчета очков
     public void sumOfPoints(){
+        //просто сумма кубиков
+        points = cube1+cube2+cube3;
         //бонусы за 2 одинаковых кубика
         if(cube1==cube2||cube2==cube3||cube1==cube3){
             pointsAndCombo += 2;
+            comboPoints = 2;
         //бонусы за 3 одинкаовых кубика
         }
         if(cube1==cube2 && cube1==cube3){
             pointsAndCombo +=1;
+            comboPoints = 3;
         }
         //бонусы за street
         if(cube1+1 == cube2 && cube1+2 == cube3 ||
@@ -74,6 +78,7 @@ public abstract class RollOfTheDice {
            cube3-1 == cube2 && cube3-2 == cube1 ||
            cube1-1 == cube2 && cube1-2 == cube3){
             pointsAndCombo +=3;
+            comboPoints = 3;
         }
         //сумма выпавших значений кубиков + бонусы, если есть
         pointsAndCombo += getCube1()+getCube2()+getCube3();
@@ -142,11 +147,7 @@ public abstract class RollOfTheDice {
                 MainActivity.getInstance().getCubeImageView3().setImageResource(R.drawable.cube6);
                 break;
         }
-            //для разработчика
-            Log.d("Бросок", "[" + getCube1() + "]" +
-                    "[" + getCube2() + "]" +
-                    "[" + getCube3() + "]" +
-                    "Points: " + getPointsAndCombo());
+
 
 
     }
@@ -154,6 +155,14 @@ public abstract class RollOfTheDice {
         public void showPoints() {
 
             MainActivity.getInstance().getScoreRound().setText(Integer.toString(pointsAndCombo));
+
+            //для разработчика
+            Log.d("Бросок", "[" + getCube1() + "]" +
+                    "[" + getCube2() + "]" +
+                    "[" + getCube3() + "]" +
+                    "  Points:" + getPoints() +
+                    "  Combo + " + getComboPoints() +
+                    "  All Points:" + getPointsAndCombo());
 
         }
     }
