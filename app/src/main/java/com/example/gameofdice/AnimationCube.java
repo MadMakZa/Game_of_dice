d32f61a8fe14364fc115511cf67c903bdb5b77e7
MadMakZa <immakza@gmail.com>
2020-12-25 12:36:36 +0300

    animation black ned refactor next time

--- app/src/main/java/com/example/gameofdice/AnimationCube.java
+++ app/src/main/java/com/example/gameofdice/AnimationCube.java
@@ -12,9 +12,12 @@ import android.widget.ImageView;
 public class AnimationCube {
 
     ObjectAnimator animationX, animationY, rotateAnimation;
-    //начальная точка броска кубиков
+    //начальная точка броска красных кубиков
     public final float yStart = 1800f;
     public final float xStart = 480f;
+    //начальная точка броска черных кубиков
+    public final float yBlackStart = -800f;
+    public final float xBlackStart = -600f;
 
     //генерация точки приземления кубиков
     public float randomGeneratorX(float a, float b){
@@ -82,13 +85,13 @@ public class AnimationCube {
     }
     //анимация первого черного кубика
     public void animationBlackFirstObject() {
-        float xEnd = randomGeneratorX(0f, 200f);
-        float yEnd = randomGeneratorY(100f, 450f);
+        float xEnd = randomGeneratorX(500f, 800f);
+        float yEnd = randomGeneratorY(500f, 850f);
         float rotate = randomRotation(380f, 750f);
 
         AnimatorSet image4 = new AnimatorSet();
-        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView4(), "x", xStart, xEnd);
-        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView4(), "y", yStart, yEnd);
+        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView4(), "x", xBlackStart, xEnd);
+        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView4(), "y", yBlackStart, yEnd);
         rotateAnimation = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView4(), "rotation", 0f, rotate);
         animationX.setDuration(1000);
         animationY.setDuration(1000);
@@ -98,13 +101,13 @@ public class AnimationCube {
     }
     //анимация второго черного кубика
     public void animationBlackTwoObject() {
-        float xEnd = randomGeneratorX(250f, 400f);
-        float yEnd = randomGeneratorY(100f, 450f);
+        float xEnd = randomGeneratorX(500f, 800f);
+        float yEnd = randomGeneratorY(500f, 800f);
         float rotate = randomRotation(280f, 750f);
 
         AnimatorSet image5 = new AnimatorSet();
-        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView5(), "x", xStart, xEnd);
-        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView5(), "y", yStart, yEnd);
+        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView5(), "x", xBlackStart, xEnd);
+        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView5(), "y", yBlackStart, yEnd);
         rotateAnimation = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView5(), "rotation", 0f, rotate);
         animationX.setDuration(1000);
         animationY.setDuration(1000);
@@ -114,13 +117,13 @@ public class AnimationCube {
     }
     //анимация третьего черного кубика
     public void animationBlackThreeObject() {
-        float xEnd = randomGeneratorX(270f, 400f);
-        float yEnd = randomGeneratorY(480f, 800f);
+        float xEnd = randomGeneratorX(500f, 800f);
+        float yEnd = randomGeneratorY(500f, 800f);
         float rotate = randomRotation(280f, 750f);
 
         AnimatorSet image6 = new AnimatorSet();
-        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView6(), "x", xStart, xEnd);
-        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView6(), "y", yStart, yEnd);
+        animationX = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView6(), "x", xBlackStart, xEnd);
+        animationY = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView6(), "y", yBlackStart, yEnd);
         rotateAnimation = ObjectAnimator.ofFloat(MainActivity.getInstance().getCubeImageView6(), "rotation", 0f, rotate);
         animationX.setDuration(1000);
         animationY.setDuration(1000);
